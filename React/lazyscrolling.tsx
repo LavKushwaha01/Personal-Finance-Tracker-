4df5946a66dd11eabd9e8d4afb22aab7e44ca2c1
lav kumar <lavkumar062@gmail.com>
2025-08-31 13:31:41 +0530

    saving done

--- React/lazyscrolling.tsx
+++ React/lazyscrolling.tsx
@@ -27,6 +27,7 @@ export default function ChartOnScroll() {
   return (
     <div ref={ref} className="w-full h-[400px] flex items-center justify-center">
       {isVisible ? (
+        
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
@@ -34,8 +35,9 @@ export default function ChartOnScroll() {
           className="mt-25 ml-30 w-320 mr-10 p-10 bg-blue-200 rounded-4xl dark:bg-neutral-900"
         >
           <ChartAreaInteractive />
-
+         
         </motion.div>
+
       ) : (
         <p className="font-bold text-3xl">Chart will load when visible...</p>
       )}
--- app/mySaving/page.tsx
+++ app/mySaving/page.tsx
@@ -1,17 +1,65 @@
-import { styleText } from "util";
+import { getServerSession } from "next-auth";
+import { Session } from "next-auth";
+import { PrismaClient } from "@prisma/client";
 
+const client = new PrismaClient();
 
+import { handler } from "@/app/api/auth/[...nextauth]/route";
+import { NextResponse } from "next/server";
 
+;
 import { ChartLineLabel } from "@/React/barchart"
 
-export default function budget(){
+
+export default async  function budget(){
+
+const session: Session | null = await getServerSession(handler);
+
+
+  if (!session?.user?.email) {
+    console.log("error here");
+    throw new Error("User not authenticated");
+  }
+
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+
+const  user = await client.user.findUnique({
+    where: {
+      email: session.user!.email!,
+    },
+    select: { id: true },
+  });
+
+  if (!user) throw new Error("user not found");
+
+    const income = await client.monthlySummary.findFirst({
+    where: {
+        userId: user.id,
+        month,
+        year,
+    },
+    select: {totalIncome: true,
+        totalExpenses: true,
+    }
+})
+
+const saving =(income?.totalIncome ?? 0) - (income?.totalExpenses ?? 0);
+
     return(
         <div>
             <div className="font-medium text-5xl mt-10 mx-20 flex  bg-blue-200 rounded-4xl dark:bg-neutral-900 p-5 pl-75 ">
-                <div>
-                Total Savings Balance =  <span>    </span>
+                <div  className="mr-7">
+                Total Savings Balance =
                 </div>
-                <div className="text-green-400"> 25000$</div>
+
+                {saving<0 ? ( 
+                     <div className="text-red-400"> <span></span>{saving}</div>
+                ) : (
+                    <div className="text-green-400"> {saving}</div>
+                )} 
+               
             </div>
             <div className="flex">
 
